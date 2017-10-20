1bc97fa9d99659f7e4269e021421ecba98fb6d0f
A13253565831 <464136046@qq.com>
2017-10-20 11:59:04 +0800

    first commit

---
+++ pages/category/index.js
@@ -0,0 +1,99 @@
+// import ApiList from  '../../config/api';
+// import request from '../../utils/request.js';
+//获取应用实例
+var app = getApp();
+var con = require('../../data.js')
+Page({
+    data: {
+        // types: null,
+      typeTree: {}, // 数据缓存
+      currType: 0,
+      // 当前类型
+      "types": [
+      ],
+      nowIndex: 0,
+      typeTree: [],
+      myHidden: false
+    },
+        
+    onLoad: function (option){
+        var that = this;
+        wx.request({
+            url: con.Index_getcategory,
+            method:'GET',
+            data: { wxappid:con.wyy_user_wxappid },
+            header: {
+                'Content-Type':  'application/json'
+            },
+            success: function (res) {
+                //--init data 
+                var status = res.data.status;
+                if(status==1) { 
+                    var list = res.data.info;
+                    var catList = res.data.first;
+                    console.log(res, catList);
+                    that.setData({
+                        types:list,
+                        typeTree:list[that.data.nowIndex],
+                        currType: list[0].id,
+                        myHidden: true
+                    });
+                } else {
+                    wx.showToast({
+                        title:res.data.err,
+                        duration:2000,
+                    });
+                }
+                    // console.log(list. currType)
+
+            },
+            error:function(e){
+                wx.showToast({
+                    title:'网络异常!',
+                    duration:2000,
+                });
+            },
+
+        });
+    },    
+ 
+    tapType: function (e){
+        var that = this;
+        // const currType = e.currentTarget.dataset.typeId;
+        var index = e.currentTarget.dataset.index; console.log(index);
+        that.setData({
+           nowIndex: index,
+           typeTree: that.data.types[index]
+        })
+        // console.log(currType);
+        // wx.request({
+        //     url: con.Index_getgoodsbycateid,
+        //     method:'GET',
+        //     data: { wxappid: con.wyy_user_wxappid,cateid:currType},
+        //     header: {
+        //         'Content-Type':  'application/json'
+        //     },
+        //     success: function (res) {
+        //         var status = res.data.status;
+        //         if(status==1) { 
+        //             var catList = res.data.info;
+        //             // console.log(catList);
+        //             that.setData({
+        //                 typeTree:catList,
+        //             });
+        //         } else {
+        //             wx.showToast({
+        //                 title:res.data.err,
+        //                 duration:2000,
+        //             });
+        //         }
+        //     },
+        //     error:function(e){
+        //         wx.showToast({
+        //             title:'网络异常！',
+        //             duration:2000,
+        //         });
+        //     }
+        // });
+    }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,383 @@
+var app = getApp();
+var con = require('../../data.js');
+Page({
+  data: {
+    imgUrls: [],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 3000,
+    duration: 1000,
+    circular: true,
+    loadingHidden: false,
+    productData: [],
+    proCat:[],
+    page: 8,
+    index: 2,
+    brand:[],
+    length: 0,
+    // 滑动
+    imgUrl: [],
+    kbs:[],
+    lastcat:[],
+    course:[],
+    youList: [],
+    navList: [],
+    adver: [],
+    jptj: [],
+    newsList: [],
+    
+  },
+
+  onLoad: function (options) {
+    var that = this;
+    wx.request({
+      url: con.Index_getongoods,
+      method:'GET',
+      data: { wxappid: con.wyy_user_wxappid, count:4},
+      header: {
+        'Content-Type':  'application/json'
+      },
+      success: function (res) {  
+        that.setData({
+          productData: res.data.info
+        });
+        setTimeout(function () {
+          that.setData({
+            loadingHidden: true
+          });
+        }, 1500)
+        //endInitData
+      },
+      fail:function(e){
+        wx.showToast({
+          title: '网络异常！',
+          duration: 2000
+        });
+      },
+    }),
+
+      wx.request({
+        url: con.Index_getongoods,
+        method: 'GET',
+        data: { wxappid: con.wyy_user_wxappid},
+        header: {
+          'Content-Type': 'application/json'
+        },
+        success: function (res) {
+          
+          that.setData({
+           length: res.data.info.length
+           
+          });
+          // console.log(length);
+          //endInitData
+        },
+        fail: function (e) {
+          wx.showToast({
+            title: '网络异常！',
+            duration: 2000
+          });
+        },
+      }),
+
+
+    wx.request({
+      url: con.Shop_getslide,
+      method: 'GET',
+      data: { wxappid: con.wyy_user_wxappid},
+      header: {
+        'Content-Type': 'application/json'
+      },
+      success: function(r){
+      
+           var img = r.data.info;
+          //  console.log(img)
+           that.setData({
+             imgUrls: img
+           })
+      }
+    })
+    this.getType();
+    this.getNewsList();
+    this.getAd();
+    this.getyouhuijuan();
+    this.newGoods();
+  },
+  // 广播信息
+  getNewsList: function() {
+    var that = this;
+    wx.request({
+      url: con.Index_getNotice,
+      method: 'GET',
+      data: {
+        wxappid: con.wyy_user_wxappid
+      },
+      header: {
+        'Content-Type': 'application/json'
+      },
+      success: function (res) {
+        // console.log(res);
+        if (res.data.status == 1) {
+          that.setData({
+            newsList: res.data.errMsg
+          })
+        } else {
+          wx.showToast({
+            title: '广播信息获取失败',
+          })
+        }
+        
+      }
+    })
+  },
+  // 广告信息
+  getAd: function(){
+    var that = this;
+    wx.request({
+      url: con.Index_getad,
+      method: 'GET',
+      data: {
+        wxappid: con.wyy_user_wxappid
+      },
+      header: {
+        'Content-Type': 'application/json'
+      },
+      success: function(res) {
+          // console.log(res);
+          var data = res.data.info;
+          that.setData({
+            adver: data
+          })
+      }
+
+    })
+
+  },
+  //  精品推荐
+   newGoods: function() {
+
+     var that = this;
+     wx.request({
+       url: con.Index_getongoods_jp,
+       method: 'GET',
+       data: {
+         wxappid: con.wyy_user_wxappid
+       },
+       header: {
+         'Content-Type': 'application/json'
+       },
+       success: function (res) {
+        //  console.log(res);
+         that.setData({
+           jptj: res.data.info
+         })
+        
+       }
+
+     })
+      
+   },
+
+  //跳转商品列表页   
+  listdetail: function (e) {
+    // console.log(e.currentTarget.dataset.title)
+    wx.navigateTo({
+      url: '../listdetail/listdetail?title=' + e.currentTarget.dataset.title,
+      success: function (res) {
+        // success
+      },
+      fail: function () {
+        // fail
+      },
+      complete: function () {
+        // complete
+      }
+    })
+  },
+  // 获取分类图标
+  getType: function () {
+    var that = this;
+    wx.request({
+      url: con.Index_getcategory,
+      method: 'GET',
+      data: { wxappid: con.wyy_user_wxappid },
+      header: {
+        'Content-Type': 'application/json'
+      },
+      success: function (res) {
+        //--init data 
+        var status = res.data.status;
+        if (status == 1) {
+          var list = res.data.info;
+          // console.log(1212, res);
+          that.setData({
+            navList: list
+          })
+
+        } else {
+          wx.showToast({
+            title: res.data.err,
+            duration: 2000,
+          });
+        }
+        // console.log(list. currType)
+
+      },
+      error: function (e) {
+        wx.showToast({
+          title: '网络异常!',
+          duration: 2000,
+        });
+      },
+
+    });
+  },
+  // 跳转筛选页面
+  goToFilter: function(e){
+    var catid = e.currentTarget.dataset.id;
+     wx.navigateTo({
+       url: '../filter/filter?catid=' + catid,
+     })
+  },
+
+  //点击加载更多
+  getMore: function (e) {
+    var that = this;
+    var page = that.data.page;
+    wx.request({
+      url: con.Index_getongoods,
+      method: 'GET',
+      data: { wxappid: con.wyy_user_wxappid, count: page },
+      header: {
+        'Content-Type': 'application/json'
+      },
+      success: function (res) {
+        // console.log(res.data.info)
+        var prolist = res.data.info.length;
+        // console.log(prolist,page);
+        if (prolist < page) {
+          wx.showToast({
+            title: '没有更多数据！',
+            duration: 2000
+          });
+          return false;
+        }
+        //that.initProductData(data);
+        that.setData({
+          page: page + 4,
+          productData: res.data.info
+        });
+        //endInitData
+      },
+      fail: function (e) {
+        wx.showToast({
+          title: '网络异常！',
+          duration: 2000
+        });
+      }
+    })
+  },
+
+  changeIndicatorDots: function (e) {
+    this.setData({
+      indicatorDots: !this.data.indicatorDots
+    })
+  },
+  changeAutoplay: function (e) {
+    this.setData({
+      autoplay: !this.data.autoplay
+    })
+  },
+  intervalChange: function (e) {
+    this.setData({
+      interval: e.detail.value
+    })
+  },
+  durationChange: function (e) {
+    this.setData({
+      duration: e.detail.value
+    })
+  },
+  // 获取优惠劵信息
+  getyouhuijuan: function(e) {
+    var that= this;
+    wx.request({
+      url: con.Index_discountlist,
+      method: 'POST',
+      data: { wxappid: con.wyy_user_wxappid },
+      header: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      success: function (r) {
+        
+        var data = r.data.list;
+        // console.log(r, data);
+
+        //时间戳的处理  
+        if (data != null) {
+          for (var i = 0; i < data.length; i++) {
+            data[i].q_begin_time = that.toDate(data[i].q_begin_time);
+            data[i].q_end_time = that.toDate(data[i].q_end_time)
+          }
+        }
+        that.setData({
+          youList: data,
+        })
+       
+      }
+    })
+  },
+  // 领取优惠劵
+  myVou: function(e){
+    var id = e.currentTarget.dataset.vid;
+    var guoqi = e.currentTarget.dataset.guoqi;
+    // console.log(id);
+    var that = this;
+    if (guoqi == 1) {
+      wx.showToast({
+        title: '优惠劵已过期'
+      })
+    } else {
+      wx.request({
+        url: con.Index_lingqu_youhui,
+        method: 'POST',
+        data: {
+          wxappid: con.wyy_user_wxappid,
+          fansid: app.globalData.fansid,
+          id: id,
+        },
+        header: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        success: function (r) {
+          var data = r.data.info;
+          if (data == 'success') {
+            wx.showToast({
+              title: '领取成功'
+            })
+          } else {
+            wx.showToast({
+              title: '不能重复领取'
+            })
+          }
+        }
+      })
+    }
+    
+  },
+
+  toDate: function (time) {
+    var n = time * 1000;
+    var date = new Date(n);
+    var Y = date.getFullYear() + '/';
+    var M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '/';
+    var D = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
+    return (Y + M + D)
+  },
+
+
+  onShareAppMessage: function () {
+    
+  }
+
+});
